9f916d96624631f98f3a1055e9db515dc9b714c8
shivigupta1379 <shivigupta1379@gmail.com>
2019-07-29 14:36:09 +0530

    changes made

---
+++ calc.java
@@ -0,0 +1,28 @@
+package project1;
+
+public class calc {
+	public int x,y;
+	public calc() {
+		x=0;
+		y=0;
+	}
+	public int add(int i,int j) {
+		return (i+j);
+	}
+	public int sub(int i,int j) {
+		return (i-j);
+	}
+	public int multiply(int i,int j) {
+		return (i*j);
+	}
+	public int divide(int i,int j) {
+		return (i/j);
+	}
+	public static void main(String[] args) {
+		//calc ob1 =new calc();
+
+		
+		//System.out.println(ob1.add(10,20));
+	}
+}
+
